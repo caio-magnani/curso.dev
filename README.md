2aedb24d6d50c1a05fa63652b86bf3c5cbcd42e1
Caio Lopes Machado Magnani <49353875+caio-magnani@users.noreply.github.com>
2023-06-24 20:31:06 +0000

    style: :art: style coding prittier

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ Repositório para os desafios do curso.dev
 ## Desafios:
 
 ### Desafio 1
-- [x] Criar um site com apenas uma frase em uma URL com uma frase que impacte uma pessoa.
+
+-   [x] Criar um site com apenas uma frase em uma URL com uma frase que impacte uma pessoa.
 
 ### Desafio 2
-- [ ] Milestone 0 - Em Construção
+
+-   [ ] Milestone 0 - Em Construção
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,8 @@
-export default function Home(){
-    return <h1>Eu precisei morrer e matar vários amores para no final descobrir que o amor é algo inevitável e imperdível.</h1>
+export default function Home() {
+    return (
+        <h1>
+            Eu precisei morrer e matar vários amores para no final descobrir que
+            o amor é algo inevitável e imperdível.
+        </h1>
+    );
 }
